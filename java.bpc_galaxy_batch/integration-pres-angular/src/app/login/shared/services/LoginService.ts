581bce8d4f13941d9da87c3fa625a0bb870bfebe
Louis Bertin NDJOMO <louisbertin.ndjomo@externe.bnpparibas.com>
2019-07-09 17:14:30 +0200

    message

--- java.bpc_galaxy_batch/integration-pres-angular/src/app/login/shared/services/LoginService.ts
+++ java.bpc_galaxy_batch/integration-pres-angular/src/app/login/shared/services/LoginService.ts
@@ -1,5 +1,5 @@
 
-import {throwError as observableThrowError,  Observable } from 'rxjs';
+import { throwError as observableThrowError, Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
@@ -16,15 +16,15 @@ export class LoginService {
   authenticate(username: string, password: string): Observable<any> {
     const headers = new Headers();
     if (username != null && password != null) {
-    const authorizationValue: string = 'Basic ' + btoa(username + ':' + password);
+      const authorizationValue: string = 'Basic ' + btoa(username + ':' + password);
       headers.append('authorization', authorizationValue);
     }
 
-    return this.http.get('user', {headers})
-          .pipe(
-            map((res: Response) => res.json()),
-            catchError( error => this.handleError(error))
-          );
+    return this.http.get('user', { headers })
+      .pipe(
+        map((res: Response) => res.json()),
+        catchError(error => this.handleError(error))
+      );
   }
 
   /**
@@ -34,10 +34,10 @@ export class LoginService {
    */
   logout(): Observable<string | {}> {
     return this.http.post('logout', {})
-            .pipe(
-               map(res => this.extractStringFromResponse(res)),
-               catchError(res => this.handleError(res))
-            );
+      .pipe(
+        map(res => this.extractStringFromResponse(res)),
+        catchError(res => this.handleError(res))
+      );
   }
 
   /**
@@ -60,19 +60,19 @@ export class LoginService {
    *
    * @param res the response to treat.
    */
-   protected handleError(error: Response | any): any {
-     let errMsg: string;
-     if (error instanceof Response) {
-       try {
-          errMsg = `${error.status} - ${error.json().message || ''}`;
-       } catch (e) {
-          errMsg = `${error.status} - ${error.text() || ''}`;
-       }
-     } else {
-       errMsg = error.message ? error.message : error.toString();
-     }
+  protected handleError(error: Response | any): any {
+    let errMsg: string;
+    if (error instanceof Response) {
+      try {
+        errMsg = `${error.status} - ${error.json().message || ''}`;
+      } catch (e) {
+        errMsg = `${error.status} - ${error.text() || ''}`;
+      }
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
 
-     return observableThrowError(errMsg);
-   }
+    return observableThrowError(errMsg);
+  }
 
 }
